b9471b28e4e315745fcf77e250250ae72791a6bc
Estudiante <Estudiante@10.203.0.173>
2018-08-13 08:30:40 -0500

    Comit inicial

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double pi=3.1415;
+        
+        // TODO code application logic here
+        System.out.println("Hola Mundo. El numero pi es igual a: ");
+        System.out.println(pi);
+    }
+    
+}
